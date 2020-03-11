0eb738bd671b4b3f8b984a5764ffee7e90a5532f
tommygod3 <tommygod1@live.com>
2020-03-11 21:24:31 +0000

    Added first pass at downloading

--- download/reddit_downloader.py
+++ download/reddit_downloader.py
@@ -1,5 +1,16 @@
-import json
+import json, csv
+import tqdm
 import praw
+
+def get_valid_title(submission):
+    if "[" not in submission.title and "|" not in submission.title:
+        return submission.title.strip().replace("\n", " ")
+
+def get_highest_valid_comment(submission):
+    for comment in list(submission.comments):
+        if "[" not in comment.body and "|" not in comment.body:
+            return comment.body.strip().replace("\n", " ")
+
 
 with open("reddit-secret.json") as reader:
     secret = json.load(reader)
@@ -10,7 +21,13 @@ reddit = praw.Reddit(client_id=secret["client_id"],
 
 subreddit = reddit.subreddit("magicTCG")
 
-for submission in subreddit.hot(limit=10):
-    print(f"Title: {submission.title}")
-    print(f"Comment: {submission.comments[0].body}")
+maximum_posts = 900
 
+with open("mtg_data.csv", "w") as writer:
+    csv_writer = csv.writer(writer, delimiter="|")
+    csv_writer.writerow(["Title", "Comment"])
+    for submission in tqdm.tqdm(subreddit.hot(limit=maximum_posts), total=maximum_posts):
+        title = get_valid_title(submission)
+        top_comment = get_highest_valid_comment(submission)
+        if not title is None and not top_comment is None:
+            csv_writer.writerow([title, top_comment])
